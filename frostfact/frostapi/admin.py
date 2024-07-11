b984a635125ebc56e9d6e7242da63964a46a80b7
taoshaus <taos.haus.thumps@gmail.com>
2024-07-10 18:20:41 -0600

    create client profile on form submissions

--- frostfact/frostapi/admin.py
+++ frostfact/frostapi/admin.py
@@ -48,14 +48,16 @@ from .models import ClientProfile, ContactFormSubmission, EventData
 class ClientProfileAdmin(admin.ModelAdmin):
     list_display = ('client_last_name', 'client_first_name', 'client_business', 'client_phone', 'client_email', 'client_event_space', 'client_special_needs')
     search_fields = ["client_first_name", "client_email"]
+    readonly_fields = ('slug',)
 
 @admin.register(ContactFormSubmission)
 class ContactFormSubmissionAdmin(admin.ModelAdmin):
     list_display = ('customer_email', 'subject', 'first_name', 'last_name', 'message', 'condition')
-    readonly_fields = ["time_stamp"]
+    readonly_fields = ["time_stamp", 'slug']
     search_fields = ["time_stamp", "customer_email"]
 
 @admin.register(EventData)
 class EventDataAdmin(admin.ModelAdmin):
     list_display = ('event_name', 'event_date', 'event_host', 'event_image', 'client_profile')
     search_fields = ['event_name', 'client_profile__client_business', 'event_host']
+    readonly_fields = ('slug',)
--- frostfact/frostapi/models.py
+++ frostfact/frostapi/models.py
@@ -3,9 +3,12 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.text import slugify
+import uuid
 
-
-
+def generate_unique_slug(value):
+    base_slug = slugify(value)
+    unique_id = uuid.uuid4().hex[:6]  # Generate a unique identifier
+    return f"{base_slug}-{unique_id}"
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -55,23 +58,24 @@ class ClientProfile(models.Model):
         null=True,
         verbose_name="Client's Special Needs"
     )
-    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Slug")
+    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Client Slug", editable=False)  # Set editable=False
 
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.client_business)
+            self.slug = generate_unique_slug(self.client_business)
         super(ClientProfile, self).save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.client_first_name} {self.client_last_name} - {self.client_business}"
 
 class ContactFormSubmission(models.Model):
-    client_profile = models.ForeignKey(
-        ClientProfile,
-        on_delete=models.CASCADE,
-        related_name='contact_submissions',
-        verbose_name="Client Profile"
-    )
+    # client_profile = models.ForeignKey(
+    #     ClientProfile,
+    #     on_delete=models.CASCADE,
+    #     related_name='contact_submissions',
+    #     verbose_name="Client Profile"
+    # )
     customer_email = models.EmailField(
         verbose_name="Customer's Email"
     )
@@ -81,6 +85,7 @@ class ContactFormSubmission(models.Model):
         null=True,
         verbose_name="Subject"
     )
+    phone = models.CharField(max_length=12, blank=True, null=True, verbose_name="Phone Number")
     first_name = models.CharField(
         max_length=255,
         blank=True,
@@ -106,33 +111,35 @@ class ContactFormSubmission(models.Model):
         null=True,
         verbose_name="Condition"
     )
-    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Slug")
+    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Contact Slug", editable=False)  # Set editable=False
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.last_name)
+            self.slug = generate_unique_slug(f'{self.last_name}-{self.first_name}' )
         super(ContactFormSubmission, self).save(*args, **kwargs)
 
     def __str__(self):
         return f"Submission by {self.customer_email} on {self.time_stamp}"
 
-    @receiver(post_save, sender=ContactFormSubmission)
-    def execute_after_save(sender, instance, created, **kwargs):
-        if created:
-            # Execute custom logic after saving ContactFormSubmission
-            instance.run_after_save()
-
     def run_after_save(self):
-        if self.client_profile_id:
-            try:
-                client_profile = self.client_profile
-            except ClientProfile.DoesNotExist:
-                client_profile = ClientProfile.objects.create()
+        # Always create a new ClientProfile instance
+        client_profile = ClientProfile.objects.create(
 
-            # Update fields in ClientProfile based on ContactFormSubmission
-            client_profile.name = f"{self.first_name} {self.last_name}"
-            client_profile.name = f"{self.first_name} {self.last_name}"
-            client_profile.save()
+            client_first_name=self.first_name,
+            client_last_name=self.last_name,
+            client_email=self.customer_email,
+            client_phone=self.phone,
+            # Add other fields as needed
+        )
+        # Associate the new ClientProfile with this ContactFormSubmission
+        # self.client_profile = client_profile
+        self.save()
+
+@receiver(post_save, sender=ContactFormSubmission)
+def execute_after_save(sender, instance, created, **kwargs):
+    if created:
+        # Execute custom logic after saving ContactFormSubmission
+        instance.run_after_save()
 
 
 class EventData(models.Model):
@@ -166,11 +173,12 @@ class EventData(models.Model):
         blank=True,
         verbose_name='Image Upload'
     )
-    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Slug")
+    slug = models.SlugField(unique=True, blank=True, null=True, verbose_name="Event Slug", editable=False)  # Set editable=False
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.event_name)
+            self.slug = generate_unique_slug(self.event_name)
         super(EventData, self).save(*args, **kwargs)
+
     def __str__(self):
         return self.event_name

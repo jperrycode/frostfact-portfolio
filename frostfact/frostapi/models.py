298f2b8b5ea279d9b569fa81bf09d19cf1522d6d
taoshaus <taos.haus.thumps@gmail.com>
2024-07-22 12:56:42 -0600

    idea

--- frostfact/frostapi/models.py
+++ frostfact/frostapi/models.py
@@ -34,7 +34,7 @@ class ClientProfile(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = generate_unique_slug(ClientProfile, f'{self.client_business}')
+            self.slug = generate_unique_slug(ClientProfile, f'{self.client_first_name}-{self.client_last_name}')
         super(ClientProfile, self).save(*args, **kwargs)
 
     def __str__(self):

99755cb7df534bfe28437e8238c7fed941855e22
taoshaus <taos.haus.thumps@gmail.com>
2024-08-11 11:07:19 -0600

    spacing

--- frostfact/frostapi/serializers.py
+++ frostfact/frostapi/serializers.py
@@ -1,22 +1,27 @@
 from rest_framework import serializers
 from .models import *
 
-class ContactFormSerializer(serializers.ModelSerializer):
 
+class ContactFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = ContactFormSubmission
         fields = '__all__'
+
 
 class EventDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventData
         fields = '__all__'
+
+
 class ClientProfileSerializer(serializers.ModelSerializer):
     contact_submissions = ContactFormSerializer(many=True, read_only=True)
     events = EventDataSerializer(many=True, read_only=True)
+
     class Meta:
         model = ClientProfile
         fields = '__all__'
+
 
 class FaqDataSerializer(serializers.ModelSerializer):
     class Meta:
@@ -35,10 +40,12 @@ class TextSliderTopSerializer(serializers.ModelSerializer):
         model = TextSliderTop
         fields = '__all__'
 
+
 class TextSliderBottomSerializer(serializers.ModelSerializer):
     class Meta:
         model = TextSliderBottom
         fields = '__all__'
+
 
 class GalleryDataSerializer(serializers.ModelSerializer):
     class Meta:
